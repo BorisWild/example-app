8bb83fa705c57b31022469f8b012be885638cce7
BorisWild <boriswild@gmail.com>
2023-11-24 21:35:50 +0300

    Update index.tsx

--- frontend/src/components/document/components/DocumentEditor/index.tsx
+++ frontend/src/components/document/components/DocumentEditor/index.tsx
@@ -1,14 +1,14 @@
 "use client"
 //@ts-ignore
-import { Editor } from "react-draft-wysiwyg";
+//import { Editor } from "react-draft-wysiwyg";
 //@ts-ignore
-import { EditorState } from 'draft-js';
+//import { EditorState } from 'draft-js';
 import {useState} from "react";
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+//import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export default function DocumentEditor(){
 
-    const [ editorState, setEditorState ] = useState( () => EditorState.createEmpty() );
+    //const [ editorState, setEditorState ] = useState( () => EditorState.createEmpty() );
 
     return (
         <div className={'flex-1 h-full px-[3rem] py-[2rem]'}>
@@ -18,12 +18,13 @@ export default function DocumentEditor(){
             <h1 className={'font-bold text-4xl mb-2'}>Welcome to Lawyer GPT!</h1>
             <p>Try generating text from Chat PT for your legal document.</p>
             <div className={'mt-3'}>
-                <Editor
-                    editorState={editorState}
-                    wrapperClassName="demo-wrapper"
-                    editorClassName="demo-editor"
-                    onEditorStateChange={setEditorState}
-                />
+               <textarea />
+                // <Editor
+               //     editorState={editorState}
+               //     wrapperClassName="demo-wrapper"
+               //     editorClassName="demo-editor"
+               //     onEditorStateChange={setEditorState}
+               // />
             </div>
         </div>
     )
