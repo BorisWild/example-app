f9a961a340498c4313fe0caf78b8a99358f3dc5d
boris <boriswild@gmail.com>
2023-12-25 00:31:06 +0300

    add landing chat ui

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "/frontend/" : undefined,
+
   images: {
     remotePatterns: [
       {
